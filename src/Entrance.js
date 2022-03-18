863a09e153080a27c03031a1d3d2578a359e3846
Anar Huseynov <47721124+AnarHuseynov00@users.noreply.github.com>
2022-03-18 04:09:57 -0400

    Add files via upload

--- src/Entrance.js
+++ src/Entrance.js
@@ -1,14 +1,29 @@
+import logo from './images/logo200.png'
+import { Link } from "react-router-dom";
+
 const Entrance = () => {
     const title = "Welcome to the new generation of machine learning models used in Finance"
     return ( 
-    <div>
+    <div className="entrance" style={{alignItems:"center"}}>
+        <div style={{width:"50%", marginLeft:"37%"}}>
+            <img src={logo} alt="Logo" />
+        </div>
         <h2>{title}</h2>
         <div className="entrance-text" style={{paddingTop: 10}}>
             <p>Usage of machine learning methods is among hot topics in Financial Engineering. Its applications ranges from 
                 clustering of different stocks according to their idiosyncratic differences, to modeling time-series financial
                 data using neural networks.
-
-                Volo-Wizard is the machine learning software, trained for predicting the volatility of {<a href="https://www.investopedia.com/terms/s/sp500.asp">{"S&P500"}</a>} index.
+            </p>
+            <p>Volo-Wizard is the ML software for predicting the volatility 
+                of {<a href="https://www.investopedia.com/terms/s/sp500.asp">{"S&P500"}</a>} index. More detailed information
+                about the literature background (motivation) of the project and machine learning algorithm used is provided
+                in the {<Link to="/motivation">Motivation</Link>} page. Instructions about how to use the software is provided
+                 in the {<Link to="/about">Documentation</Link>} page</p>
+            <p>
+                The software is developed 
+                by {<a href="https://www.linkedin.com/in/anar-huseynov-78b585151/">{"Anar Huseynov"}</a>}, a graduate 
+                student at the University of Michigan and also, an incoming
+                Machine Learning Operations engineer at the Ford Motor Company. 
             </p>
         </div>
 
--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,29 @@
 import { useState } from "react";
+import Button from '@mui/material/Button';
+import LineChart from 'react-linechart';
+import '../node_modules/react-linechart/dist/styles.css';
+
 
 const Home = () => {
     const title = "Volatily Prediction App"
     const [inputData, setInputData] = useState('')
-    const [prediction, setPrediction] = useState("")
+    const [prediction, setPrediction] = useState(null)
     const [buttonDisable, setButtonDisable] = useState(false);
     const [buttonText, setButtonText] = useState("Evaluate")
     const [predictionText, setPredictionText] = useState("Enter values to see the prediction")
-    
+    const [chartData, setChartData] = useState([])
+
+    const convertToNum = (val, index) =>{
+        let num_val = Number(val);
+        num_val = Math.abs(num_val)
+        return {x:index, y:num_val}
+    }
     const HandleSubmit = (event) =>{
         event.preventDefault();
         setButtonDisable(true);
         setButtonText("Evaluating...");
         setPredictionText("Calculating prediction...")
-        setPrediction("")
+        setPrediction(null)
         console.log("making post request");
         fetch('https://4w801pm2t0.execute-api.us-east-1.amazonaws.com/prod/', {
             method: 'POST',
@@ -32,7 +42,16 @@ const Home = () => {
             // POST request success
             else {
               const outputBytesData = JSON.parse(data.body)['outputResultsData'];
-              setPrediction(outputBytesData);
+              let num_return = Number(outputBytesData)
+              setPrediction(num_return);
+              let strNums = inputData.split(",");
+              let vol_numbers = strNums.map(convertToNum)
+              let cdata = [{
+                color: "steelblue", 
+                points: vol_numbers
+              }]
+              cdata[0].points.push({x: cdata[0].points.length, y: num_return})
+              setChartData(cdata)
             }
       
             // re-enable submit button
@@ -50,12 +69,20 @@ const Home = () => {
             <h1>
                 {title}
             </h1>
-            <form onSubmit={HandleSubmit}>
-                <label>Input Field</label>
+            <form onSubmit={HandleSubmit} style={{paddingTop:20, paddingBottom:40}}>
+                <label>Enter your return values:</label>
                 <input type="text" required = {true} value={inputData} onChange={(e)=>{setInputData(e.target.value)}}/>
-                <button type="submit" disabled={buttonDisable}>{buttonText}</button>
+                <Button type="submit" disabled={buttonDisable}>{buttonText}</Button>
             </form>
-            <h4>{predictionText} {prediction}</h4>
+            <h4 style={{paddingBottom:10}}>{predictionText} {prediction}</h4>
+            <p style={{paddingBottom:15}}>Line Graph. Prediction is the last value</p>
+            <LineChart 
+                    xLabel = {'time frame'}
+                    yLabel = {'volatility in %'}
+                        width={500}
+                        height={400}
+                        data={chartData}
+                    />
         </div>
 
      );
